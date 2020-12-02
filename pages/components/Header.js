594ecb6a6447cc5bf381742411f6c32df86655fe
CGill98 <coopergill4@gmail.com>
2020-12-02 17:08:04 +0000

    First Header Completed

---
+++ pages/components/Header.js
@@ -0,0 +1,32 @@
+import styles from '../../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+        <div className={styles.header}>
+          <h1>San Remo Italian Restaurant</h1>
+          <ul className={styles.headerul}>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/menu">
+                <a>Menu</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/gallery">
+                <a>Gallery</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact">
+                <a>Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+  )
+}
